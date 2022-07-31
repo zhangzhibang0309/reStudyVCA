354abe2d15d8b5e9380ad213f4d066ad15d45773
zhangzhibang <zhangzhibang@exiao.tech>
2022-07-31 16:38:48 +0800

    feat(feat vca): reactive相关函数

--- 01_learn_vca/src/03_setup的其他函数/App.vue
+++ 01_learn_vca/src/03_setup的其他函数/App.vue
@@ -30,6 +30,16 @@ export default {
       info.name = payload;
     }
 
+    // reactive相关的api（reactive是proxy对象）
+    /**
+     * isProxy 判断是否是reactive或者readonly对象
+     * isReactive 见名知意义
+     * isReadonly 见名知意义
+     * toRaw 拿到原始对象，非代理
+     * shallowReactive 浅层reactive，不代理嵌套对象
+     * shallowReadonly 浅层readonly,同上
+     */
+
     return {
       info,
       changeInfoName,
