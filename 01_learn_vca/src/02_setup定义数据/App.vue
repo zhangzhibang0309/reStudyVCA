64e0f87da35df9eced1e70c67b4d43f1c592c307
zhangzhibang <zhangzhibang@exiao.tech>
2022-07-31 13:25:08 +0800

    feat(feat vca: 数据响应式

---
+++ 01_learn_vca/src/02_setup定义数据/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <h2>{{ message }}</h2>
+  </div>
+
+  <div>
+    <button @click="changeAcount">点击更改</button>
+    <p>{{ account.username }}</p>
+    <p>{{ account.password }}</p>
+  </div>
+</template>
+
+<script>
+import { reactive } from "vue";
+
+export default {
+  setup() {
+    // 1.定义普通的数据
+    // 非响应式
+    const message = "hello world";
+    function changeMessage() {
+      message = "hello";
+      console.log(message);
+    }
+
+    // 2.定义响应式数据
+    // 2.1reactive函数 定义复杂类型
+    const account = reactive({
+      username: "asd",
+      password: "123",
+    });
+    function changeAcount() {
+      account.username = "ewq";
+      account.password = "321";
+    }
+
+    return {
+      message,
+      changeMessage,
+      account,
+      changeAcount,
+    };
+  },
+};
+</script>
+
+<style></style>
--- 01_learn_vca/src/main.js
+++ 01_learn_vca/src/main.js
@@ -1,4 +1,4 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./02_setup定义数据/App";
 
-createApp(App).mount('#app')
+createApp(App).mount("#app");
